d70c24422df48d23dd845aa383bf60894c064f0e
Anne Fouilloux <annefou@geo.uio.no>
2018-10-11 15:33:14 +0200

    added comments to help users

--- automate_run.bash
+++ automate_run.bash
@@ -1,12 +1,18 @@
 # define variables
 
-# result directory wher you will save your results
+# result directory where you will save your results
 
 RESULT_DIR=/drives/C/users/annefou/data-shell/results
 
+# input directory
 INPUT_DIR=/drives/C/users/annefou/data-shell/molecules
 
+# do not update anything below
+
+# remove directory
 rm -rf $RESULT_DIR
+
+# Create result directory
 mkdir $RESULT_DIR
 
 for filename in $INPUT_DIR/*.pdb
